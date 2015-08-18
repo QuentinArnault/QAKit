28072004f6cca32f18237969e31da9221481843c
Quentin ARNAULT <quentin.arnault@niji.fr>
2015-08-18 11:15:40 +0200

    [I] add possibilty to set image rendering mode from InterfaceBuilder for UIImageView

---
+++ QAKit/QAKit/UIKit/UIImageView+kit.h
@@ -0,0 +1,15 @@
+//
+//  UIImageView+kit.h
+//  QAKit
+//
+//  Created by Quentin Arnault on 18/08/2015.
+//  Copyright (c) 2015 Quentin Arnault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (kit)
+
+- (void)QA_setImageRenderingMode:(UIImageRenderingMode)renderMode;
+
+@end
---
+++ QAKit/QAKit/UIKit/UIImageView+kit.m
@@ -0,0 +1,20 @@
+//
+//  UIImageView+kit.m
+//  QAKit
+//
+//  Created by Quentin Arnault on 18/08/2015.
+//  Copyright (c) 2015 Quentin Arnault. All rights reserved.
+//
+
+#import "UIImageView+kit.h"
+
+@implementation UIImageView (kit)
+
+#pragma mark -
+- (void)QA_setImageRenderingMode:(UIImageRenderingMode)renderMode
+{
+    NSAssert(self.image, @"Image must be set before setting rendering mode");
+    self.image = [self.image imageWithRenderingMode:renderMode];
+}
+
+@end
