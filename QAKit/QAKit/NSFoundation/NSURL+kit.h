930f6d2360ad3c91623d3c87361df85c92e6c6aa
Quentin ARNAULT <quentin.arnault@niji.fr>
2015-08-20 10:21:04 +0200

    [I] add a convenient method to extract parameters from an NSURL

---
+++ QAKit/QAKit/NSFoundation/NSURL+kit.h
@@ -0,0 +1,15 @@
+//
+//  NSURL+kit.h
+//  QAKit
+//
+//  Created by Quentin Arnault on 20/08/2015.
+//  Copyright (c) 2015 Quentin Arnault. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURL (kit)
+
+- (NSDictionary *)QA_parameters;
+
+@end
---
+++ QAKit/QAKit/NSFoundation/NSURL+kit.m
@@ -0,0 +1,29 @@
+//
+//  NSURL+kit.m
+//  QAKit
+//
+//  Created by Quentin Arnault on 20/08/2015.
+//  Copyright (c) 2015 Quentin Arnault. All rights reserved.
+//
+
+#import "NSURL+kit.h"
+
+@implementation NSURL (kit)
+
+#pragma mark -
+- (NSDictionary *)QA_parameters {
+    NSArray *parameters = [request.URL.query componentsSeparatedByString:@"&"];
+    
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    for (NSString *parameter in parameters) {
+        NSArray *comp = [parameter componentsSeparatedByString:@"="];
+        if (comp.count == 2) {
+            [dict setObject:[comp lastObject]
+                     forKey:[comp firstObject]];
+        }
+    }
+    
+    return [NSDictionary dictionaryWithDictionary:dict];
+}
+
+@end
